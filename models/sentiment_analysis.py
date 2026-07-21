5537bde15b733d81c81d2b1130a7ae1ee3ed2cff
Aravind Reddy <kunareddyaravindreddy2@gmail.com>
2026-07-21 18:57:02 +0530

    Update sentiment_analysis.py

--- models/sentiment_analysis.py
+++ models/sentiment_analysis.py
@@ -3,14 +3,16 @@ from transformers import pipeline
 
 @st.cache_resource
 def load_sentiment_model():
-    return pipeline("sentiment-analysis")
+    return pipeline(
+        "sentiment-analysis",
+        model="lxyuan/distilbert-base-multilingual-cased-sentiments-student"
+    )
 
 sentiment_model = load_sentiment_model()
 
 def analyze_sentiment(message):
-    result = sentiment_model(
+    return sentiment_model(
         message,
         truncation=True,
         max_length=512
     )
-    return result
